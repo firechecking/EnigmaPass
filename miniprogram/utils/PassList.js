d3530f62ee670a988bf4a296f3e33771bf39a4bc
Patrick <patrick.zzz@alibaba-inc.com>
2022-01-28 22:17:35 +0800

    bugfix: MAX_LIMIT in reading database should be lower than 20

--- miniprogram/utils/PassList.js
+++ miniprogram/utils/PassList.js
@@ -41,14 +41,16 @@ class PassList {
     this.decodeDatabase(this.password)
   }
   async _downloadCollection(colname) {
+    // 下载数据列表
+    // 微信限制云开发一次只能获取20条记录，所以进行分批获取，MAX_LIMIT设为20
     const db = wx.cloud.database()
-    const MAX_LIMIT = 100
+    const MAX_LIMIT = 20
     const countResult = await db.collection(colname).count()
     const total = countResult.total
     if (total < 1)
       return { data: [] }
     // 计算需分几次取
-    const batchTimes = Math.ceil(total / 100)
+    const batchTimes = Math.ceil(total / MAX_LIMIT)
     // 承载所有读操作的 promise 的数组
     const tasks = []
     for (let i = 0; i < batchTimes; i++) {
